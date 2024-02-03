28752f50dd4836ce2196171cc2145ab19685bc09
Vladimir Petrigo <vladimir.petrigo@gmail.com>
2024-02-03 22:26:39 +0100

    server: Add simple server implementation
    Index page returns the dummy list of chatters

---
+++ src/server.rs
@@ -0,0 +1,91 @@
+use std::fmt::{Formatter, Write};
+use std::fs;
+use std::io::Read;
+use std::path::Path;
+
+use serde::Serialize;
+use serde_json::Value;
+use tinytemplate::TinyTemplate;
+
+#[derive(Serialize, Debug)]
+struct Content<'a, T> {
+    chatters_list: &'a [T],
+}
+
+struct ServerError {
+    kind: String,
+    message: String,
+}
+
+type Result<T> = std::result::Result<T, ServerError>;
+
+impl From<std::io::Error> for ServerError {
+    fn from(value: std::io::Error) -> Self {
+        ServerError {
+            kind: String::from("io"),
+            message: value.to_string(),
+        }
+    }
+}
+
+impl From<tinytemplate::error::Error> for ServerError {
+    fn from(value: tinytemplate::error::Error) -> Self {
+        ServerError {
+            kind: String::from("template"),
+            message: value.to_string(),
+        }
+    }
+}
+
+pub(crate) fn run_server() {
+    rouille::start_server("0.0.0.0:12345", move |request| {
+        rouille::router!(request,
+            (GET) (/) => {
+                if let Ok(page) = generate_delimited_text() {
+                    return rouille::Response::html(page)
+                }
+
+                rouille::Response::empty_404()
+            },
+            _ => rouille::Response::empty_404()
+        )
+    });
+}
+
+fn generate_delimited_text() -> Result<String> {
+    let dummy = ["Alex", "Bob", "Alice", "wer1346", "skylinego"];
+    let template = read_index_template()?;
+
+    add_chatters_to_index_page(&dummy, template)
+}
+
+fn read_index_template() -> Result<String> {
+    let file_path = Path::new("./public/index.template.html");
+    let mut file = fs::File::open(file_path)?;
+    let mut buffer = String::new();
+
+    file.read_to_string(&mut buffer)?;
+
+    Ok(buffer)
+}
+
+fn add_chatters_to_index_page<T: Serialize>(chatters_list: &[T], index_template: String) -> Result<String> {
+    let mut tt = TinyTemplate::new();
+    let context = Content {
+        chatters_list
+    };
+
+    tt.add_template("index", index_template.as_str())?;
+    tt.add_formatter("index", chatter_name_formatter);
+
+    Ok(tt.render("index", &context)?)
+}
+
+fn chatter_name_formatter(name: &Value, out: &mut String) -> tinytemplate::error::Result<()> {
+    if let Value::String(str) = name {
+        out.write_str(str)?;
+        out.write_char('\n')?;
+    }
+
+    Ok(())
+}
