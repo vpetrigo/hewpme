971c37de7b22b696ea3cc4010cefa825587e94be
Vladimir Petrigo <vladimir.petrigo@gmail.com>
2024-02-04 19:11:34 +0100

    html: Add basic styling of the chatters list

---
+++ public/animate.js
@@ -0,0 +1,22 @@
+window.onload = function () {
+    let container = document.getElementById("container");
+    const containerHeight = container.offsetHeight;
+    const windowHeight = window.innerHeight;
+    const creditsHeight = Math.ceil(containerHeight / windowHeight) * -100
+    const animationDuration = containerHeight / windowHeight * 10000;
+
+    container.animate([
+            {
+                // from
+                top: '105%',
+            },
+            {
+                // to
+                top: `${creditsHeight}%`,
+            },
+        ],
+        {
+            duration: animationDuration,
+            iterations: Infinity
+        });
+}
