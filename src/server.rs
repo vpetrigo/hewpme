bd670ae67b6209f3f3fcef4f4bf5aabecae63959
Vladimir Petrigo <vladimir.petrigo@gmail.com>
2024-02-14 23:11:05 +0100

    server: Update HTML page template generation context

--- src/server.rs
+++ src/server.rs
@@ -14,15 +14,40 @@ struct Content<T>
 where
     T: IntoIterator,
 {
-    chatters_list: T,
-    followers_list: T,
-    subscribers_list: T,
+    chatters_list: Option<T>,
+    followers_list: Option<T>,
+    subscribers_list: Option<T>,
 }
 
 #[derive(Debug)]
 struct ServerError {
     kind: String,
     message: String,
+}
+
+#[derive(Debug)]
+struct TemplateContext<T: IntoIterator + Serialize> {
+    chatters: Option<T>,
+    followers: Option<T>,
+    subscribers: Option<T>,
+}
+
+impl<T: IntoIterator + Serialize + Clone> TemplateContext<T> {
+    fn new(chatters_list: T, followers_list: T, subscriber_list: T) -> Self {
+        let c = chatters_list.clone().into_iter().count();
+        let f = followers_list.clone().into_iter().count();
+        let s = subscriber_list.clone().into_iter().count();
+
+        let chatters = if c > 0 { Some(chatters_list) } else { None };
+        let followers = if f > 0 { Some(followers_list) } else { None };
+        let subscribers = if s > 0 { Some(subscriber_list) } else { None };
+
+        return TemplateContext {
+            chatters,
+            followers,
+            subscribers,
+        };
+    }
 }
 
 type Result<T> = std::result::Result<T, ServerError>;
@@ -72,14 +97,10 @@ pub(crate) fn run_server(chatters_list: ChattersList, event_list: SafeTwitchEven
     });
 }
 
-fn generate_credits_text<T: IntoIterator + Serialize>(
-    chatters_list: T,
-    followers_list: T,
-    subscribers_list: T,
-) -> Result<String> {
+fn generate_credits_text<T: IntoIterator + Serialize>(ctx: TemplateContext<T>) -> Result<String> {
     let template = read_index_template()?;
 
-    add_chatters_to_index_page(chatters_list, followers_list, subscribers_list, template)
+    add_chatters_to_index_page(ctx, template)
 }
 
 fn read_index_template() -> Result<String> {
@@ -93,16 +114,14 @@ fn read_index_template() -> Result<String> {
 }
 
 fn add_chatters_to_index_page<T: IntoIterator + Serialize>(
-    chatters_list: T,
-    followers_list: T,
-    subscribers_list: T,
+    ctx: TemplateContext<T>,
     index_template: String,
 ) -> Result<String> {
     let mut tt = TinyTemplate::new();
     let context = Content {
-        chatters_list,
-        followers_list,
-        subscribers_list,
+        chatters_list: ctx.chatters,
+        followers_list: ctx.followers,
+        subscribers_list: ctx.subscribers,
     };
 
     tt.add_template("index", index_template.as_str())?;
@@ -110,7 +129,11 @@ fn add_chatters_to_index_page<T: IntoIterator + Serialize>(
     tt.add_formatter("subscribers", chatter_name_formatter);
     tt.add_formatter("chatters", chatter_name_formatter);
 
-    Ok(tt.render("index", &context)?)
+    let result = tt.render("index", &context);
+
+    println!("{:?}", result);
+
+    result.map_err(|e| e.into())
 }
 
 fn chatter_name_formatter(name: &Value, out: &mut String) -> tinytemplate::error::Result<()> {
@@ -135,5 +158,14 @@ fn generate_credit_page(
     let guard3 = event_list.get_subscribers();
     let subs = guard3.iter().collect::<Vec<_>>();
 
-    generate_credits_text(chatters, followers, subs)
+    let template_context = TemplateContext::<Vec<&String>>::new(chatters, followers, subs);
+
+    generate_credits_text(template_context)
 }
+//
+// fn get_template_context<T>(
+//     chatters: Vec<&String>,
+//     followers: Vec<&String>,
+//     subs: Vec<&String>,
+// ) -> TemplateContext<T> {
+// }
