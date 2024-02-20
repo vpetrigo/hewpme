8d4c9fe714c5eae1341a70b31afc7049b1a37e7b
Vladimir Petrigo <vladimir.petrigo@gmail.com>
2024-02-20 20:55:24 +0100

    app: eventsub: Clean up User ID and Debug feature

--- src/eventsub.rs
+++ src/eventsub.rs
@@ -1,9 +1,13 @@
-use std::str::FromStr;
+use core::str::FromStr;
+use std::env;
 
 use twitch_api::helix::HelixClient;
+use twitch_api::types::UserId;
+use twitch_oauth2::UserToken;
 use url::Url;
 
 use crate::helper::SafeTwitchEventList;
+use crate::utils::get_user_token;
 use crate::websocket;
 
 const TEST_WEBSOCKET_URL: &str = "ws://127.0.0.1:8080/ws";
@@ -11,38 +15,39 @@ const TEST_WEBSOCKET_URL: &str = "ws://127.0.0.1:8080/ws";
 // moderator:read:followers channel:read:subscriptions
 pub(crate) async fn run_eventsub_client(event_list: SafeTwitchEventList) {
     let client = HelixClient::<reqwest::Client>::new();
+    let channel_name =
+        env::var("TWITCH_CHANNEL").expect("Please specify Twitch channel name to connect to");
+    let token = get_user_token().await;
 
-    // let token = twitch_oauth2::UserToken::from_token(
-    //     client.get_client(),
-    //     std::env::var("TWITCH_USER_TOKEN").unwrap().into(),
-    // )
-    // .await
-    // .unwrap();
-    let token: twitch_oauth2::UserToken = twitch_oauth2::UserToken::from_existing_unchecked(
-        std::env::var("TWITCH_USER_TOKEN").unwrap(),
-        None,
-        std::env::var("TWITCH_CLIENT_ID").unwrap(),
-        Some(twitch_oauth2::ClientSecret::new(
-            std::env::var("TWITCH_CLIENT_SECRET").unwrap(),
-        )),
-        std::env::var("TWITCH_LOGIN").unwrap().into(),
-        std::env::var("TWITCH_USER_ID").unwrap().into(),
-        Some(vec![
-            twitch_oauth2::Scope::ModeratorReadFollowers,
-            twitch_oauth2::Scope::ChannelReadSubscriptions,
-        ]),
-        Some(std::time::Duration::from_secs(21600)),
-    );
-    let ws = websocket::WebsocketClient::new(
-        None,
-        token,
-        client,
-        From::from("662136860"),
-        Url::from_str(TEST_WEBSOCKET_URL).unwrap(),
-        event_list,
-    );
+    let connection_url = if cfg!(feature = "debug") {
+        Url::from_str(TEST_WEBSOCKET_URL).unwrap()
+    } else {
+        Url::from_str(twitch_api::TWITCH_EVENTSUB_WEBSOCKET_URL.as_str()).unwrap()
+    };
+    let user_id: UserId = if cfg!(feature = "debug") {
+        From::from("123456")
+    } else {
+        get_user_id(&client, &token, &channel_name).await
+    };
+
+    let ws =
+        websocket::WebsocketClient::new(None, token, client, user_id, connection_url, event_list);
 
     ws.run()
         .await
         .expect("Websocket client finished its execution");
+}
+
+async fn get_user_id<'a, C: 'a>(
+    client: &'a HelixClient<'a, C>,
+    token: &UserToken,
+    user_name: &str,
+) -> UserId
+where
+    C: twitch_api::HttpClient,
+{
+    match client.get_user_from_login(user_name, token).await {
+        Ok(user_id) => user_id.unwrap().id,
+        Err(e) => panic!("Unable to get User ID from Twitch: {e}"),
+    }
 }
