40ce654a4c1727cc9a1eced41f4d2dde32e7815b
Vladimir Petrigo <vladimir.petrigo@gmail.com>
2024-02-21 01:09:37 +0100

    app: utils: token: Clean up token management interface

--- src/utils/token.rs
+++ src/utils/token.rs
@@ -1,33 +1,104 @@
 use core::time::Duration;
 use std::collections::HashMap;
 use std::fmt::Formatter;
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 use std::{env, fs, io};
 
-use reqwest::ClientBuilder;
+use chrono::{DateTime, Utc};
+use reqwest::{ClientBuilder, IntoUrl};
 use serde::{Deserialize, Serialize};
 use tokio::runtime::Handle;
 use tokio::sync::{OnceCell, RwLock};
+use twitch_irc::login::UserAccessToken;
 use twitch_oauth2::{
-    AccessToken, ClientSecret, CsrfToken, RefreshToken, TwitchToken, UserToken, UserTokenBuilder,
+    AccessToken, ClientSecret, CsrfToken, RefreshToken, Scope, TwitchToken, UserToken,
+    UserTokenBuilder,
 };
 use url::Url;
 
+use crate::config::{get_app_directory_path, get_eventsub_config_file};
 use crate::utils::{create_auth_channel, run_auth_server};
 
+pub struct TokenWrapper {
+    token: UserToken,
+}
+
+impl TokenWrapper {
+    pub async fn new<T: IntoUrl>(ctx: TokenCreateContext<'_, T>) -> Self {
+        TokenWrapper {
+            token: request_user_token(ctx).await,
+        }
+    }
+
+    pub fn get_user_token(&self) -> &UserToken {
+        &self.token
+    }
+}
+
 #[derive(Serialize, Deserialize)]
-struct Token {
-    access_token: AccessToken,
-    refresh_token: Option<RefreshToken>,
-    expires_in: Duration,
+pub struct Token {
+    pub access_token: AccessToken,
+    pub refresh_token: Option<RefreshToken>,
+    pub created_at: DateTime<Utc>,
+    pub valid_till: DateTime<Utc>,
+    pub scopes: Option<Vec<Scope>>,
+}
+
+impl From<UserToken> for Token {
+    fn from(value: UserToken) -> Self {
+        value.into()
+    }
 }
 
 impl From<&UserToken> for Token {
     fn from(value: &UserToken) -> Self {
+        let now = chrono::offset::Utc::now();
+        let valid_till = now + value.expires_in();
+
         Self {
             access_token: value.access_token.clone(),
             refresh_token: value.refresh_token.clone(),
-            expires_in: value.expires_in(),
+            created_at: now,
+            valid_till,
+            scopes: Some(value.scopes().to_vec()),
+        }
+    }
+}
+
+impl From<UserAccessToken> for Token {
+    fn from(value: UserAccessToken) -> Self {
+        value.into()
+    }
+}
+
+impl From<&UserAccessToken> for Token {
+    fn from(value: &UserAccessToken) -> Self {
+        let valid_till = value
+            .expires_at
+            .unwrap_or(value.created_at + Duration::from_secs(600));
+
+        Self {
+            access_token: value.access_token.clone().into(),
+            refresh_token: Some(value.refresh_token.clone().into()),
+            created_at: value.created_at,
+            valid_till,
+            scopes: None,
+        }
+    }
+}
+
+pub struct TokenCreateContext<'a, T: IntoUrl> {
+    pub scopes: &'a [Scope],
+    pub force_verify: bool,
+    pub redirect_url: T,
+}
+
+impl<'a, T: IntoUrl> TokenCreateContext<'a, T> {
+    pub fn new(scopes: &'a [Scope], force_verify: bool, redirect_url: T) -> Self {
+        TokenCreateContext {
+            scopes,
+            force_verify,
+            redirect_url,
         }
     }
 }
@@ -50,68 +121,70 @@ impl core::fmt::Display for Error {
     }
 }
 
-const APP_NAME: &str = "hewpme";
-const CONFIG_FILE_NAME: &str = "config.json";
-const SCOPES: &[twitch_oauth2::scopes::Scope] = &[
-    twitch_oauth2::scopes::Scope::ChannelBot,
-    twitch_oauth2::scopes::Scope::ChannelModerate,
-    twitch_oauth2::scopes::Scope::ChatRead,
-    twitch_oauth2::scopes::Scope::ChatEdit,
-    twitch_oauth2::scopes::Scope::ChannelReadSubscriptions,
-    twitch_oauth2::scopes::Scope::ModeratorReadFollowers,
-];
+impl Token {
+    pub fn save(&self, out: PathBuf) -> io::Result<()> {
+        let file = fs::File::create(out)?;
+        let writer = io::BufWriter::new(file);
 
-static TOKEN_STORAGE: OnceCell<RwLock<UserToken>> = OnceCell::const_new();
+        serde_json::to_writer(writer, &self)?;
 
-pub async fn get_user_token() -> UserToken {
-    TOKEN_STORAGE
-        .get_or_init(|| async {
-            let bot_config_dir = directories::BaseDirs::new()
-                .unwrap()
-                .config_dir()
-                .join(APP_NAME);
-            let bot_config_file = bot_config_dir.join(CONFIG_FILE_NAME);
+        Ok(())
+    }
 
-            if bot_config_dir.exists() && bot_config_file.exists() && bot_config_file.is_file() {
-                // load token from the file
-                return RwLock::new(restore_token_from_file(bot_config_file).await);
-            }
+    pub fn from_file(file: PathBuf) -> io::Result<Self> {
+        Ok(get_token_from_file(file)?)
+    }
 
-            RwLock::new(request_user_authentication().await)
-        })
-        .await
-        .read()
-        .await
-        .clone()
+    pub async fn into_user_token(self) -> UserToken {
+        let client_secret = env::var("TWITCH_CLIENT_SECRET").unwrap();
+        let client = ClientBuilder::new()
+            .redirect(reqwest::redirect::Policy::none())
+            .build()
+            .expect("Unable to build a client to send request to Twitch API");
+        let token = UserToken::from_existing(
+            &client,
+            self.access_token,
+            self.refresh_token,
+            ClientSecret::from(client_secret),
+        )
+            .await;
+
+        match token {
+            Err(e) => panic!("Unable to get token: {e}"),
+            Ok(token) => token,
+        }
+    }
 }
+
+const SCOPES: &[Scope] = &[
+    Scope::ChannelBot,
+    Scope::ChannelModerate,
+    Scope::ChatRead,
+    Scope::ChatEdit,
+    Scope::ChannelReadSubscriptions,
+    Scope::ModeratorReadFollowers,
+];
 
 pub fn _update_user_token(_token: &str) {
     todo!()
 }
 
-fn get_token_from_file(config_file: PathBuf) -> Token {
+fn get_token_from_file(config_file: PathBuf) -> io::Result<Token> {
     // TODO: add handling of existing configuration directory, but file is missed
-    let file = match fs::File::open(config_file) {
-        Err(e) => panic!("Unable to open file: {e}"),
-        Ok(file) => file,
-    };
+    let file = fs::File::open(config_file)?;
     let reader = io::BufReader::new(file);
-    let result = serde_json::from_reader(reader);
 
-    match result {
-        Err(e) => panic!("Failed to get token from file: {e}"),
-        Ok(token) => token,
-    }
+    Ok(serde_json::from_reader(reader)?)
 }
 
-fn create_token_context() -> UserTokenBuilder {
-    let redirect_url = Url::parse("http://localhost:3000/auth/twitch/callback").unwrap();
+fn create_token_context<T: IntoUrl>(ctx: TokenCreateContext<'_, T>) -> UserTokenBuilder {
+    let redirect_url = ctx.redirect_url.into_url().expect("Invalid redirect URL");
     let client_id = env::var("TWITCH_CLIENT_ID").unwrap();
     let client_secret = env::var("TWITCH_CLIENT_SECRET").unwrap();
     let mut builder = UserTokenBuilder::new(client_id, client_secret, redirect_url);
 
-    builder = builder.set_scopes(SCOPES.to_vec());
-    builder = builder.force_verify(true); // Defaults to false
+    builder = builder.set_scopes(ctx.scopes.to_vec());
+    builder = builder.force_verify(ctx.force_verify); // Defaults to false
 
     builder
 }
@@ -120,18 +193,21 @@ fn generate_token_url(builder: &mut UserTokenBuilder) -> (Url, CsrfToken) {
     builder.generate_url()
 }
 
-fn verify_csrf_token(response: &HashMap<String, String>, csrf_token: &str) -> Result<(), Error> {
+fn verify_csrf_token(
+    response: &HashMap<String, String>,
+    builder: &UserTokenBuilder,
+) -> Result<(), Error> {
     let resp_csrf_token = response.get("state");
 
-    if resp_csrf_token.is_none() {
-        return Err(Error::NoCsrfToken);
+    if let Some(csrf) = resp_csrf_token {
+        return if builder.csrf_is_valid(resp_csrf_token.unwrap()) {
+            Ok(())
+        } else {
+            Err(Error::CsrfTokenMismatch)
+        };
     }
 
-    if resp_csrf_token.unwrap() != csrf_token {
-        return Err(Error::CsrfTokenMismatch);
-    }
-
-    Ok(())
+    Err(Error::NoCsrfToken)
 }
 
 fn extract_pair<'a>(
@@ -158,28 +234,7 @@ fn extract_url(query: &HashMap<String, String>) -> Result<(String, String), ()> 
     }
 }
 
-async fn restore_token_from_file(config_file: PathBuf) -> UserToken {
-    let client_secret = env::var("TWITCH_CLIENT_SECRET").unwrap();
-    let token = get_token_from_file(config_file);
-    let client = ClientBuilder::new()
-        .redirect(reqwest::redirect::Policy::none())
-        .build()
-        .expect("Unable to build a client to send request to Twitch API");
-    let token = UserToken::from_existing(
-        &client,
-        token.access_token,
-        token.refresh_token,
-        ClientSecret::from(client_secret),
-    )
-        .await;
-
-    match token {
-        Err(e) => panic!("Unable to get token: {e}"),
-        Ok(token) => token,
-    }
-}
-
-async fn request_user_authentication() -> UserToken {
+async fn request_user_token<T: IntoUrl>(ctx: TokenCreateContext<'_, T>) -> UserToken {
     // no token - retrieve it from Twitch API
     // 1. run auth server
     // 2. generate token URL
@@ -191,7 +246,7 @@ async fn request_user_authentication() -> UserToken {
     let auth_server = handle.spawn(async move {
         run_auth_server(tx).await;
     });
-    let mut token_context = create_token_context();
+    let mut token_context = create_token_context(ctx);
     let (url, csrf_token) = generate_token_url(&mut token_context);
     // Make your user navigate to this URL, for example
     println!("Visit this URL to authorize Twitch access: {}", url);
@@ -203,7 +258,8 @@ async fn request_user_authentication() -> UserToken {
         handle.block_on(auth_server).unwrap();
     });
     println!("{auth_response:?}, {}", csrf_token.as_str());
-    if let Err(e) = verify_csrf_token(&auth_response, csrf_token.as_str()) {
+
+    if let Err(e) = verify_csrf_token(&auth_response, &token_context) {
         tracing::error!("CSRF token error: {e}");
         panic!("CSRF token error: {e}");
     }
@@ -218,29 +274,11 @@ async fn request_user_authentication() -> UserToken {
                 .get_user_token(&client, state, code)
                 .await
                 .expect("Failed to get user token from Twitch");
-            println!("{token:?}");
-            save_token_data(&token);
+            // println!("{token:?}");
+            // save_token_data(&token);
 
             token
         }
         _ => todo!(),
     }
-}
-
-fn save_token_data(token: &UserToken) {
-    let bot_config_dir = directories::BaseDirs::new()
-        .unwrap()
-        .config_dir()
-        .join(APP_NAME);
-
-    if !bot_config_dir.exists() {
-        fs::create_dir(&bot_config_dir).expect("Unable to create bot config directory");
-    }
-
-    let config_file = bot_config_dir.join(CONFIG_FILE_NAME);
-    let file = fs::File::create(config_file).expect("Unable to create bot configuration file");
-    let writer = io::BufWriter::new(file);
-    let token_to_store: Token = token.into();
-
-    serde_json::to_writer(writer, &token_to_store).expect("Failed to save bot token info");
 }
