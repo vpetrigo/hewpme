3901608a4886b6477c2152d7fe64fe16bf45c6bc
Vladimir Petrigo <vladimir.petrigo@gmail.com>
2024-02-21 01:08:14 +0100

    app: chat: Update chat token handling

--- src/chat/mod.rs
+++ src/chat/mod.rs
@@ -1,26 +1,75 @@
 /// Requires the following permissions:
 /// - channel:read:subscriptions
 /// - moderator:read:followers
-use std::env;
+use std::{env, io};
 
-use twitch_irc::login::StaticLoginCredentials;
+use async_trait::async_trait;
+use twitch_irc::login::{
+    RefreshingLoginCredentials, StaticLoginCredentials, TokenStorage, UserAccessToken,
+};
 use twitch_irc::message::ServerMessage::Privmsg;
 use twitch_irc::{ClientConfig, SecureTCPTransport, TwitchIRCClient};
+use twitch_oauth2::Scope;
 
+use crate::config;
 use crate::helper::ChattersList;
-use crate::utils::get_user_token;
+use crate::utils::{Token, TokenCreateContext, TokenHandler};
+
+#[derive(Debug)]
+struct ChatTokenStorage;
+
+#[async_trait]
+impl TokenStorage for ChatTokenStorage {
+    type LoadError = io::Error;
+    type UpdateError = io::Error;
+
+    async fn load_token(&mut self) -> Result<UserAccessToken, Self::LoadError> {
+        let chat_config = config::get_chat_config_file();
+        let token = match Token::from_file(chat_config.clone()) {
+            Err(_) => {
+                let scopes = [Scope::ChatRead, Scope::ChatEdit];
+                let token_create_ctx =
+                    TokenCreateContext::new(&scopes, false, config::REDIRECT_URL);
+                let token_handler = TokenHandler::new(token_create_ctx).await;
+
+                token_handler.get_user_token().into()
+            }
+            Ok(token) => token,
+        };
+
+        token.save(chat_config)?;
+
+        Ok(UserAccessToken {
+            access_token: token.access_token.clone().take(),
+            refresh_token: token.refresh_token.clone().unwrap().take(),
+            created_at: token.created_at,
+            expires_at: Some(token.valid_till),
+        })
+    }
+
+    async fn update_token(&mut self, token: &UserAccessToken) -> Result<(), Self::UpdateError> {
+        let chat_config = config::get_chat_config_file();
+        Ok(Token::from(token).save(chat_config)?)
+    }
+}
 
 pub async fn run_twitch_irc_client(chatters_list: ChattersList) {
     // default configuration is to join chat as anonymous.
-    let token = get_user_token().await;
-    let config = ClientConfig::new_simple(StaticLoginCredentials::new(
-        token.login.take().to_lowercase(),
-        Some(token.access_token.take()),
-    ));
-    let (mut incoming_messages, client) =
-        TwitchIRCClient::<SecureTCPTransport, StaticLoginCredentials>::new(config);
 
-    let client_clone = client.clone();
+    let storage = ChatTokenStorage {};
+
+    let credentials = RefreshingLoginCredentials::init(
+        config::get_client_id(),
+        config::get_client_secret(),
+        storage,
+    );
+    let config = ClientConfig::new_simple(credentials);
+    let (mut incoming_messages, client) = TwitchIRCClient::<
+        SecureTCPTransport,
+        RefreshingLoginCredentials<ChatTokenStorage>,
+    >::new(config);
+
+    let responder = client.clone();
     // first thing you should do: start consuming incoming messages,
     // otherwise they will back up.
     let join_handle = tokio::spawn(async move {
@@ -30,6 +79,11 @@ pub async fn run_twitch_irc_client(chatters_list: ChattersList) {
                     .lock()
                     .await
                     .insert(user_msg.sender.name.clone());
+                // TODO: Add some funny commands handling
+                responder
+                    .say_in_reply_to(user_msg, "Hello".into())
+                    .await
+                    .unwrap();
             }
 
             tracing::trace!("Received message: {:?}", message);
