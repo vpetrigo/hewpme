e5fbdc57fac65e55b9d70704d663959dda19bc75
Vladimir Petrigo <vladimir.petrigo@gmail.com>
2024-02-21 21:19:17 +0100

    app: eventsub: Add token retrieving

--- src/eventsub/mod.rs
+++ src/eventsub/mod.rs
@@ -3,39 +3,58 @@ use std::env;
 
 use twitch_api::helix::HelixClient;
 use twitch_api::types::UserId;
-use twitch_oauth2::UserToken;
+use twitch_oauth2::{Scope, UserToken};
 use url::Url;
 
 use crate::helper::SafeTwitchEventList;
-// use crate::utils::get_user_token;
-use crate::websocket;
+use crate::utils::{Token, TokenCreateContext, TokenWrapper};
+use crate::{config, websocket};
 
 const TEST_WEBSOCKET_URL: &str = "ws://127.0.0.1:8080/ws";
 
 // moderator:read:followers channel:read:subscriptions
 pub(crate) async fn run_eventsub_client(event_list: SafeTwitchEventList) {
-    // let client = HelixClient::<reqwest::Client>::new();
-    // let channel_name =
-    //     env::var("TWITCH_CHANNEL").expect("Please specify Twitch channel name to connect to");
-    // let token = get_user_token().await;
-    //
-    // let connection_url = if cfg!(feature = "debug") {
-    //     Url::from_str(TEST_WEBSOCKET_URL).unwrap()
-    // } else {
-    //     Url::from_str(twitch_api::TWITCH_EVENTSUB_WEBSOCKET_URL.as_str()).unwrap()
-    // };
-    // let user_id: UserId = if cfg!(feature = "debug") {
-    //     From::from("123456")
-    // } else {
-    //     get_user_id(&client, &token, &channel_name).await
-    // };
-    //
-    // let ws =
-    //     websocket::WebsocketClient::new(None, token, client, user_id, connection_url, event_list);
-    //
-    // ws.run()
-    //     .await
-    //     .expect("Websocket client finished its execution");
+    let config_file = config::get_eventsub_config_file();
+    let token = match Token::from_file(config_file.clone()) {
+        Err(_) => {
+            let scopes = [
+                Scope::ModeratorReadFollowers,
+                Scope::ModeratorManageBannedUsers,
+                Scope::ChannelReadSubscriptions,
+            ];
+            let token_create_ctx = TokenCreateContext::new(&scopes, false, config::REDIRECT_URL);
+            let token_handler = TokenWrapper::new(token_create_ctx).await;
+
+            token_handler.get_user_token().into()
+        }
+        Ok(token) => token,
+    };
+
+    token
+        .save(config_file)
+        .expect("Unable to save EventSub token");
+    let token = token.into_user_token().await;
+    let client = HelixClient::<reqwest::Client>::new();
+    let channel_name =
+        env::var("TWITCH_CHANNEL").expect("Please specify Twitch channel name to connect to");
+
+    let connection_url = if cfg!(feature = "debug") {
+        Url::from_str(TEST_WEBSOCKET_URL).unwrap()
+    } else {
+        Url::from_str(twitch_api::TWITCH_EVENTSUB_WEBSOCKET_URL.as_str()).unwrap()
+    };
+    let user_id: UserId = if cfg!(feature = "debug") {
+        From::from("123456")
+    } else {
+        get_user_id(&client, &token, &channel_name).await
+    };
+
+    let ws =
+        websocket::WebsocketClient::new(None, token, client, user_id, connection_url, event_list);
+
+    ws.run()
+        .await
+        .expect("Websocket client finished its execution");
 }
 
 async fn get_user_id<'a, C: 'a>(
